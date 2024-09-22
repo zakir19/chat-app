cc87c98058cc6a36a22bdb3e452e7a8fd0bced6b
Zakir <patelzakir784@gmail.com>
2024-09-22 15:15:40 +0530

    x

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
-import Testimonials from "./sections/Testimonials.jsx";
 import Download from "./sections/Download.jsx";
 import Footer from "./sections/Footer.jsx";
 
